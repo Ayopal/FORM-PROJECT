1da3fb0b918491b8f66efbeb3e942cee337bbf6d
Ayoola Paul <Ayopaul19@gmail.com>
2020-06-10 10:38:34 +0100

    Javascript code for project

---
+++ app.js
@@ -0,0 +1,31 @@
+/*** FORM CONTROLLER */
+(function formController() {
+
+
+
+
+
+})()
+
+
+
+
+/*** UI CONTROLLER */
+(function uiController () {
+
+
+
+})()
+
+
+
+
+/*** GLOBAL CONTROLLER */
+(function globalController (fCtrl, uiCtrl) {
+
+
+
+    
+
+
+})(formController, uiController)
